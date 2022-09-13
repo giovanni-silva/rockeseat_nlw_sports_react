283778ae7b751ac92acec7b7395c0847f1879155
giovanniNative <giovanni.native@gmail.com>
2022-09-13 14:11:48 -0300

    base building

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rockeseat_nlw_sports_react
+
+Stage 1/6 | Aula Prática - Base Building
+
+Criei a pasta server como back-end ;
+
+server.mjs : 'mjs' em vez '.js' por causa para mudar a importação do express , (moduleJavascript)
+
+Dentro da pasta server, criou o arquivo package.json: vai ser aonde vão ficar a referência para as dependências que a gente instala na nosso aplicação.
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (request, response) => {
+  return response.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+  ]);
+});
+
+// localhost:3333/ads
+// request serve para buscar informações que estão vindo requisição
+// reponse serve para eu devolver uma resposta
+
+// podemos fazer a requisição e validar se está passando ou não pelo Insomnia ou Postman ou Hoppscotch
+
+app.listen(3333);
